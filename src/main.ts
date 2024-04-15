cf56e4e190af09f66b5565a0420e78a42c196083
Lukas Hajda <lukas.hajda@demi.sk>
2024-04-15 12:01:40 +0200

    reset password (reset-password)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/saga-green/theme.css'
+import { createApp } from 'vue'
+
+import { createPinia } from 'pinia'
 import Button from "primevue/button";
+import InputText from 'primevue/inputtext';
 import App from './App.vue'
 import router from './router'
 
@@ -13,6 +15,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
-app.component('PrimeButton', Button)
+app.component('Button', Button)
+app.component('InputText', InputText)
 
 app.mount('#app')
---
+++ src/views/auth/ResetPassword.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const value = ref();
+
+
+</script>
+
+<template>
+  <h1 class="text-red-600 text-bold">KOKOS</h1>
+  <div class="flex flex-column gap-2">
+    <label for="username">Email</label>
+    <InputText id="username" v-model="value" aria-describedby="username-help" />
+    <small id="username-help">Enter your username to reset your password.</small>
+  </div>
+  <Button label="Submit" />
+</template>
